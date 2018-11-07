de13907391629db33ad7497e49ddf1951ceee70f
Sully Adelard <dev1.bvom@outlook.fr>
2018-11-07 14:33:06 +0100

    Code synchrone & async

--- scriptsDivers.js
+++ scriptsDivers.js
@@ -1,3 +1,30 @@
+var fs = require('fs');
+var cache = {};
+
+//Le défaut de la function ci-dessous est qu'il est dans un cas synchrone et dans un autre asynchrone
+function inconsistentRead(filename, callback) {
+    if (cache[filename]) {
+        //invoked synchronously
+        callback(cache[filename]);
+    } else {
+        //asynchronous function
+        fs.readFile(filename, 'utf8', function (err, data) {
+            cache[filename] = data;
+            callback(data);
+        });
+    }
+}
+
+//SOLUTION 1 : Rendre synchrone dans tous les cas
+function consistentReadSync(filename) {
+    if (cache[filename]) {
+        return cache[filename];
+    } else {
+        cache[filename] = fs.readFileSync(filename, 'utf8');
+        return cache[filename];
+    }
+}
+
 function createFileReader(filename) {
     var listeners = [];
     inconsistentRead(filename, function (value) {
@@ -11,3 +38,14 @@ function createFileReader(filename) {
         }
     };
 }
+
+var reader1 = createFileReader("toto.txt");
+
+reader1.onDataReady(function (data) {
+    console.log("First call data : " + data);
+
+    var reader2 = createFileReader("toto1.txt");
+    reader2.onDataReady(function (data) {
+        console.log("Second call data : " + data);
+    });
+});
